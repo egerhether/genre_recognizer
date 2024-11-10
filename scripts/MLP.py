b8d34110c33dd1f422c615b0fa7fdac6649219f9
Izabela Kurek <ikurek0309@gmail.com>
2024-11-10 09:56:09 +0100

    provisional MLP model done, file structure

---
+++ scripts/MLP.py
@@ -0,0 +1,81 @@
+import torch.nn as nn
+
+class MLP(nn.Module):
+    """
+    This class implements a Multi-layer Perceptron in PyTorch.
+    It handles the different layers and parameters of the model.
+    Once initialized an MLP object can perform forward.
+    """
+
+    def __init__(self, n_inputs, n_hidden, n_classes, use_batch_norm = False, dropout = 0.2):
+        """
+        Initializes MLP object.
+
+        Args:
+          n_inputs: number of inputs.
+          n_hidden: list of ints, specifies the number of units
+                    in each linear layer. If the list is empty, the MLP
+                    will not have any linear layers, and the model
+                    will simply perform a multinomial logistic regression.
+          n_classes: number of classes of the classification problem.
+                     This number is required in order to specify the
+                     output dimensions of the MLP
+          use_batch_norm: If True, add a Batch-Normalization layer in between
+                          each Linear and ELU layer.
+        """
+
+        super(MLP, self).__init__()
+
+        layers = []
+        n_hidden.append(n_classes)
+
+        input_layer = nn.Linear(n_inputs, n_hidden)
+        nn.init.kaiming_normal_(input_layer.weight)
+        layers.append(input_layer)
+
+        for i, n in enumerate(n_hidden):
+            
+            if i == len(n_hidden) - 1:
+                break
+            
+            if use_batch_norm:
+                layers.append(nn.BatchNorm1d(n_hidden[i]))
+
+            layers.append(nn.ReLU())
+
+            if dropout > 0:
+                layers.append(nn.Dropout(dropout))
+
+            layer = nn.Linear(n, n_hidden[i + 1])
+            nn.init.kaiming_normal_(layer.weight)
+            nn.init.constant_(layer.bias, 0)
+
+            layers.append(layer)
+        
+        self.layers = nn.Sequential(*layers)
+
+          
+
+    def forward(self, x):
+        """
+        Performs forward pass of the input. Here an input tensor x is transformed through
+        several layer transformations.
+
+        Args:
+          x: input to the network
+        Returns:
+          out: outputs of the network
+        """
+
+        out = x
+        for layer in self.layers:
+            out = layer(out)
+
+        return out
+
+    @property
+    def device(self):
+        """
+        Returns the device on which the model is. 
+        """
+        return next(self.parameters()).device
